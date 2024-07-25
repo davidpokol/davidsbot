fc19575e82ba28683125590d74bd20ce72c96320
davidpokol <davidpokolol@gmail.com>
2024-07-25 11:38:37 +0200

    added basic authentication to the homestation commands

--- src/util/ApiUtil.ts
+++ src/util/ApiUtil.ts
@@ -1,5 +1,11 @@
+const credentials = process.env.ENCODED_BASIC_CREDENTIALS;
+
 export async function sendGetRequestWithTimeout(url: string): Promise<Response> {
+
     return await fetch(url, {
+        headers: new Headers({
+            "Authorization": `Basic ${credentials}`
+        }),
         signal: AbortSignal.timeout(5_000)
     }).catch(() => null);
 }
@@ -7,11 +13,11 @@ export async function sendGetRequestWithTimeout(url: string): Promise<Response> 
 export async function sendPostRequestWithTimeout(url: string, data: any): Promise<Response> {
 
     return await fetch(url, {
-        signal: AbortSignal.timeout(5_000),
         method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
+        headers: new Headers({
+            "Authorization": `Basic ${credentials}`
+        }),
+        signal: AbortSignal.timeout(5_000),
         body: JSON.stringify(data)
     }).catch(() => null);
 }
